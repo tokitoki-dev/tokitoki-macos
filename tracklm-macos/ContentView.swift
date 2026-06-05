42075c7847359e2afbe6aed2abbd013fe0d0b156
akarineren <akarineren@gmail.com>
2026-06-05 17:33:07 +0900

    Initial Commit

---
+++ tracklm-macos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tracklm-macos
+//
+//  Created by Eren on 2026/06/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tracklm-macos/tracklm_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tracklm_macosApp.swift
+//  tracklm-macos
+//
+//  Created by Eren on 2026/06/05.
+//
+
+import SwiftUI
+
+@main
+struct tracklm_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
